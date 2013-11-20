2fc84aad2297c92ac3f9d1d9b38e7badfc18dc48
Brad Rhodes <bradr@iqmetrix.com>
2013-11-20 17:02:23 -0600

    Initial commit

---
+++ PureSeeder.Core/Settings/CustomSettingsProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureSeeder.Core.Settings
+{
+    public interface ISettingsProvider<T> where T: new()
+    {
+        T GetSettings(bool freshCopy = false);
+        void SaveSettings(T settings);
+        IEnumerable<SettingsProvider.SettingDescriptor> ReadSettingMetaData();
+        T ResetToDefaults();
+    }
+
+    class CustomSettingsProvider<T> : ISettingsProvider<T> where T: new()
+    {
+        private readonly ISettingsProvider _innerSettingsProvider;
+
+        public CustomSettingsProvider(ISettingsProvider innerSettingsProvider)
+        {
+            if (innerSettingsProvider == null) throw new ArgumentNullException("innerSettingsProvider");
+            _innerSettingsProvider = innerSettingsProvider;
+        }
+
+        public T GetSettings(bool freshCopy = false)
+        {
+            return _innerSettingsProvider.GetSettings<T>();
+        }
+
+        public void SaveSettings(T settings)
+        {
+            SaveSettings(settings);
+        }
+
+        public IEnumerable<SettingsProvider.SettingDescriptor> ReadSettingMetaData()
+        {
+            return _innerSettingsProvider.ReadSettingMetadata<T>();
+        }
+
+        public T ResetToDefaults()
+        {
+            return _innerSettingsProvider.ResetToDefaults<T>();
+        }
+    }
+}
---
+++ PureSeeder.Core/Settings/PureSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PureSeeder.Core.Settings
+{
+    public class PureSettings
+    {
+        [DisplayName("Server Addresses")]
+        public List<string> ServerAddresses { get; set; }
+
+        [DisplayName("Minimum Players")]
+        public int MinimumPlayers { get; set; }
+
+        [DisplayName("Maximum Players")]
+        public int MaximumPlayers { get; set; }
+
+        [DisplayName("SleepWhenNotSeeding")]
+        public int SleepWhenNotSeeding { get; set; }
+
+        [DisplayName("SleepWhenSeeding")]
+        public int SleepWhenSeeding { get; set; }
+
+        [DisplayName("Enable Logging")]
+        public bool EnableLogging { get; set; }
+
+        [DisplayName("Enable Game Hang Protection")]
+        public bool EnableGameHangProtection { get; set; }
+    }
+}
