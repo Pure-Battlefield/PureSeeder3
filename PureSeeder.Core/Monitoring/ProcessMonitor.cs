57faa36bb7bcc328ae5752a06e5e3b9de5fe9f92
Brad Rhodes <bradr@iqmetrix.com>
2013-12-16 16:53:30 -0600

    Starting to add threading stuff to monitor the game in the background.

---
+++ PureSeeder.Core/Monitoring/ProcessMonitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PureSeeder.Core.Monitoring
+{
+    public delegate void ProcessStateChangeHandler(object sender, ProcessStateChangeEventArgs e);
+
+    public class ProcessMonitor
+    {
+        // Need a couple of custom events to attach to
+        public event ProcessStateChangeHandler OnProcessStateChanged; 
+
+        public async void CheckOnProcess(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+                {
+                    // Check for the process every few seconds
+                    // If it's running, trigger idle kick avoidance
+                    // If state changes from running or to running, trigger an event
+                });
+        }
+
+
+    }
+
+    public class ProcessStateChangeEventArgs : EventArgs
+    {
+        public bool IsRunning { get; set; }
+    }
+}
