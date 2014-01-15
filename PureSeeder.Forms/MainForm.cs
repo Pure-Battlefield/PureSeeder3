49bf0271057230d61a3cd3d10af2ad08f5e843f2
Brad Rhodes <rhodes.brad@gmail.com>
2014-01-15 17:26:54 -0600

    Cleaned up a bit

--- PureSeeder.Forms/MainForm.cs
+++ PureSeeder.Forms/MainForm.cs
@@ -63,7 +63,6 @@ namespace PureSeeder.Forms
             LoadBattlelog();
 
             geckoWebBrowser1.DocumentCompleted += BrowserChanged;
-            //_context.OnContextUpdate += ContextUpdated;
 
             SetRefreshTimer();
 
@@ -186,13 +185,6 @@ namespace PureSeeder.Forms
             UpdateContext();
         }
 
-        // Deprecated
-//        private void ContextUpdated(object sender, EventArgs e)
-//        {
-//            AttemptKick();
-//            AttemptSeeding();
-//        }
-
         #endregion EventHandlers
 
         #region BattlelogManipulation
@@ -202,13 +194,7 @@ namespace PureSeeder.Forms
             // Todo: This should be moved into configuration so it can more easily be changed
             const string jsCommand = "document.getElementsByClassName('btn btn-primary btn-large large arrow')[0].click()";
 
-            // Deprecated
-//            using (var context = new AutoJSContext(geckoWebBrowser1.Window.JSContext))
-//            {
-//                context.EvaluateScript(jsCommand);
-//            }
-
-            RunJavascript(jsCommand)/*.RunSynchronously()*/;
+            RunJavascript(jsCommand);
 
             _context.JoinServer();
         }
@@ -223,7 +209,7 @@ namespace PureSeeder.Forms
             if (cb.Items.Count == 0)
                 return Constants.DefaultUrl;
 
-            var address = _context.CurrentServer.Address; //((Server) cb.SelectedItem).Address;
+            var address = _context.CurrentServer.Address; 
 
             return address;
         }
@@ -234,13 +220,11 @@ namespace PureSeeder.Forms
             if (selectedUrl == String.Empty)
                 selectedUrl = Constants.DefaultUrl;
 
-            //geckoWebBrowser1.Navigate(selectedUrl);
             await Navigate(selectedUrl);
         }
 
         private Task Navigate(string url)
         {
-            //this.BeginInvoke(new Action(() => geckoWebBrowser1.Navigate(url)));
             return Task.Factory.StartNew(() => this.BeginInvoke(new Action(() => geckoWebBrowser1.Navigate(url))));
         }
 
@@ -284,27 +268,24 @@ namespace PureSeeder.Forms
             currentLoggedInUser.ForeColor = _context.GetUserStatus() == UserStatus.Correct ? Color.Green : Color.Red;
         }
 
-        private void AutoLogin(/*Action successfulLogin = null, Action failedLogin = null*/)
+        private void AutoLogin()
         {
             if (!_context.Settings.AutoLogin)
                 return;
 
             if (_context.GetUserStatus() == UserStatus.Correct)
                 return;
-                /*if(successfulLogin != null)
-                    successfulLogin.Invoke();*/
+                
 
             if (_context.GetUserStatus() == UserStatus.Incorrect)
             {
-                // Do not autologout
-                //AutoLogout(/*() => AutoLogin(successfulLogin, failedLogin), null*/);
                 return;
             }
             
-            Login(/*successfulLogin, failedLogin*/);
+            Login();
         }
 
-        private async void Login(/*Action successfulLogin = null, Action failedLogin = null*/)
+        private async void Login()
         {
             SetStatus("Attempting login.");
 
@@ -312,9 +293,6 @@ namespace PureSeeder.Forms
             string jsCommand = String.Format("$('#base-login-email').val('{0}');$('#base-login-password').val('{1}');$('#baseloginpersist').val() == '1';$(\"[name='submit']\").click();", email.Text, password.Text);
 
             RunJavascript(jsCommand);
-
-            // Check for a while if the login worked
-            //await CheckLogin(successfulLogin, failedLogin);
 
             await Sleep(1);
             SetStatus("");
@@ -329,43 +307,9 @@ namespace PureSeeder.Forms
             }
         }
 
-//        private Task RunJavascript(string javascript)
-//        {
-//            return Task.Factory.StartNew(() =>
-//                {
-//                    using (var context = new AutoJSContext(geckoWebBrowser1.Window.JSContext))
-//                    {
-//                        context.EvaluateScript(javascript);
-//                    }
-//                });
-//        }
-
-//        private Task CheckLogin(Action successfulLogin = null, Action failedLogin = null)
-//        {
-//            return Task.Factory.StartNew(() =>
-//                {
-//                    const int loginCheckCount = 10;
-//                    for (var i = 0; i < loginCheckCount; i++)
-//                    {
-//                        if (_context.Session.CurrentLoggedInUser != Constants.NotLoggedInUsername)
-//                        {
-//                            if (successfulLogin != null)
-//                                successfulLogin.Invoke();
-//                            return;
-//                        }
-//
-//                        Thread.Sleep(1000); // Sleep for 1 second
-//                    }
-//                    if (failedLogin != null)
-//                        failedLogin.Invoke();
-//                });
-//        }
-
-        private /*async*/ void Logout(/*Action successfulLogout, Action failedLogout*/)
+        private void Logout()
         {
             geckoWebBrowser1.Navigate("http://battlelog.battlefield.com/bf4/session/logout/");
-
-            /*await CheckLogout(successfulLogout, failedLogout);*/
         }
 
         private Task CheckLogout(Action successfulLogout, Action failedLogout)
@@ -390,19 +334,6 @@ namespace PureSeeder.Forms
                 });
         }
 
-        private void AutoLogout(/*Action logoutSuccess, Action logoutFail*/)
-        {
-            if (_context.GetUserStatus() == UserStatus.None)
-            {
-                /*if (logoutSuccess != null)
-                    logoutSuccess.Invoke();*/
-
-                return;
-            }
-
-            Logout(/*logoutSuccess, logoutFail*/);
-        }
-
         private bool ShouldSeed()
         {
             var shouldSeed = _context.ShouldSeed();
@@ -412,7 +343,7 @@ namespace PureSeeder.Forms
                 if (shouldSeed.Reason == ShouldNotSeedReason.NotLoggedIn)
                 {
                     SetStatus("Cannot seed. Not logged in.", 5);
-                    AutoLogin(/*RefreshPageAndData*/);
+                    AutoLogin();
                     return false;
                 }
 
@@ -425,7 +356,6 @@ namespace PureSeeder.Forms
                 if (shouldSeed.Reason == ShouldNotSeedReason.IncorrectUser)
                 {
                     SetStatus("Cannot seed. Incorrect logged in user.", 5);
-                    //AutoLogout(/*() => AutoLogin(RefreshPageAndData), null*/);
                     return false;
                 }
 
@@ -450,36 +380,32 @@ namespace PureSeeder.Forms
             return true;
         }
 
-        private /*Task*/ void AttemptKick()
+        private void AttemptKick()
         {
-            // Deprecated
-            /*return Task.Factory.StartNew(() =>
-            {*/
-                var shouldKick = _context.ShouldKick();
-                if (shouldKick.Result)
+            var shouldKick = _context.ShouldKick();
+            if (shouldKick.Result)
+            {
+                if (shouldKick.Reason == KickReason.AboveSeedingRange)
                 {
-                    if (shouldKick.Reason == KickReason.AboveSeedingRange)
-                    {
-                        var result =
-                            MessageBoxEx.Show(
-                                "Player count above max threshold. If game is running it will be stopped in 5 seconds.",
-                                "Max Player Threshold Exceeded", MessageBoxButtons.OKCancel,
-                                MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, 5000);
+                    var result =
+                        MessageBoxEx.Show(
+                            "Player count above max threshold. If game is running it will be stopped in 5 seconds.",
+                            "Max Player Threshold Exceeded", MessageBoxButtons.OKCancel,
+                            MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, 5000);
 
-                        if (result == DialogResult.OK)
-                            _context.StopGame();
-                        
-                        return;
-                    }
+                    if (result == DialogResult.OK)
+                        _context.StopGame();
 
-                    if (shouldKick.Reason == KickReason.NoServerDefined)
-                    {
-                        return;
-                    }
-
-                    throw new NotImplementedException("Need to handle all reasons for kicking.");
+                    return;
                 }
-            /*});*/
+
+                if (shouldKick.Reason == KickReason.NoServerDefined)
+                {
+                    return;
+                }
+
+                throw new NotImplementedException("Need to handle all reasons for kicking.");
+            }
         }
 
         #endregion BattlelogManipulation
@@ -488,7 +414,6 @@ namespace PureSeeder.Forms
 
         private void serverSelector_SelectionChangeCommitted(object sender, EventArgs e)
         {
-            //LoadPage();
             AnyRefresh();
         }
 
@@ -499,13 +424,11 @@ namespace PureSeeder.Forms
 
         private void joinServerButton_Click(object sender, EventArgs e)
         {
-            //JoinServer();
             AttemptSeeding();
         }
 
         private void geckoWebBrowser1_DomContentChanged(object sender, DomEventArgs e)
         {
-            //UpdateContext();
             BrowserChanged(sender, e);
         }
 
